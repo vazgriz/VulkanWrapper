2ec7f631dd27a46354d830e5c9b4f2302289bfaf
Unknown <vazgriz@gmail.com>
2018-03-03 15:47:41 -0600

    Add CreateInfo base class

---
+++ CreateInfo.cpp
@@ -0,0 +1,37 @@
+#include "include/VulkanWrapper/CreateInfo.h"
+
+vk::CreateInfo::CreateInfo() {
+    next = nullptr;
+}
+
+void Align(size_t& size, size_t alignment) {
+    size_t unalign = size % alignment;
+
+    if (unalign == 0) {
+        return;
+    } else {
+        size += alignment - unalign;
+    }
+}
+
+std::vector<char> vk::CreateInfo::Marshal(const vk::CreateInfo& info) {
+    size_t totalSize = 0;
+    std::vector<char> buffer(info.GetSize());
+
+    const CreateInfo* current = &info;
+    while (true) {
+        size_t index = totalSize;
+        current->Write(&buffer[index]);
+
+        current = current->next;
+        if (current != nullptr) {
+            totalSize += current->GetSize();
+            Align(totalSize, 16);
+            buffer.reserve(totalSize);
+        } else {
+            break;
+        }
+    }
+
+    return buffer;
+}
---
+++ include/VulkanWrapper/CreateInfo.h
@@ -0,0 +1,23 @@
+#pragma once
+#include <vector>
+
+namespace vk {
+    class CreateInfo {
+    public:
+        CreateInfo();
+
+        virtual size_t GetSize() const = 0;
+        virtual void Write(void* ptr) const = 0;
+        static std::vector<char> Marshal(const CreateInfo& info);
+
+        CreateInfo * next;
+    };
+
+    template<typename T>
+    class CreateInfo_ : public CreateInfo {
+    public:
+        size_t GetSize() const {
+            return sizeof(T);
+        }
+    };
+}

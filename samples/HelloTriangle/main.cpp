677c8b859aeb53717d0e7aa88df91ec6641e0ecd
Unknown <vazgriz@gmail.com>
2018-03-03 17:55:28 -0600

    Add HelloTriangle skeleton

--- samples/HelloTriangle/main.cpp
+++ samples/HelloTriangle/main.cpp
@@ -1,7 +1,41 @@
 #include <iostream>
 #include <string>
+#include <GLFW/glfw3.h>
+
+class HelloTriangle {
+public:
+    int width;
+    int height;
+    GLFWwindow* window;
+
+    HelloTriangle(GLFWwindow* window, int width, int height) {
+        this->window = window;
+    }
+
+    void run() {
+        mainLoop();
+    }
+
+    void init() {
+
+    }
+
+    void mainLoop() {
+        while (!glfwWindowShouldClose(window)) {
+            glfwPollEvents();
+        }
+    }
+};
 
 int main() {
-    std::cout << "Hello world!\n";
+    glfwInit();
+    glfwWindowHint(GLFW_CLIENT_API, GLFW_NO_API);
+    GLFWwindow* window = glfwCreateWindow(800, 600, "Hello Triangle", nullptr, nullptr);
+
+    HelloTriangle app(window, 800, 600);
+    app.run();
+
+    glfwDestroyWindow(window);
+    glfwTerminate();
     return 0;
 }

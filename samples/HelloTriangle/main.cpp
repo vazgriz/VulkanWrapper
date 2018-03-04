3cac41b13fd8dd1a5bf72c10ebd275d11ce2033e
Unknown <vazgriz@gmail.com>
2018-03-03 18:28:48 -0600

    Add Instance creation

--- samples/HelloTriangle/main.cpp
+++ samples/HelloTriangle/main.cpp
@@ -1,6 +1,12 @@
 #include <iostream>
 #include <string>
+#include <memory>
 #include <GLFW/glfw3.h>
+#include <VulkanWrapper/Instance.h>
+
+const std::vector<std::string> validationLayers = {
+    "VK_LAYER_LUNARG_standard_validation"
+};
 
 class HelloTriangle {
 public:
@@ -8,16 +14,62 @@ public:
     int height;
     GLFWwindow* window;
 
+    std::unique_ptr<vk::Instance> instance;
+
     HelloTriangle(GLFWwindow* window, int width, int height) {
         this->window = window;
     }
 
     void run() {
+        createInstance();
         mainLoop();
     }
 
-    void init() {
+    bool checkValidationSupport() {
+        auto& availableLayers = vk::Instance::availableLayers();
+        for (auto& requested : validationLayers) {
+            bool found = false;
 
+            for (auto& available : availableLayers) {
+                if (available.layerName == requested) {
+                    found = true;
+                    break;
+                }
+            }
+
+            if (!found) return false;
+        }
+
+        return true;
+    }
+
+    void createInstance() {
+        vk::ApplicationInfo appInfo = {};
+        appInfo.applicationName = "Hello Triangle";
+        appInfo.applicationVersion = VK_MAKE_VERSION(1, 0, 0);
+        appInfo.engineVersion = VK_MAKE_VERSION(1, 0, 0);
+        appInfo.apiVersion = VK_API_VERSION_1_0;
+
+        uint32_t glfwExtensionCount = 0;
+        const char** glfwExtensions;
+
+        glfwExtensions = glfwGetRequiredInstanceExtensions(&glfwExtensionCount);
+
+        //GLFW already provides the extensions in a format that the Vulkan API can accept
+        std::vector<std::string> extensions(glfwExtensionCount);
+        for (uint32_t i = 0; i < glfwExtensionCount; i++) {
+            extensions[i] = glfwExtensions[i];
+        }
+
+        vk::InstanceCreateInfo info = {};
+        info.applicationInfo = &appInfo;
+        info.enabledExtensionNames = extensions;
+
+        if (checkValidationSupport()) {
+            info.enabledLayerNames = validationLayers;
+        }
+
+        instance = std::make_unique<vk::Instance>(info);
     }
 
     void mainLoop() {

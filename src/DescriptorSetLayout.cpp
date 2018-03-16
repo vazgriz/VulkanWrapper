d025b2a11c0a9f7902e64e63127512ca8d6c3af3
Unknown <vazgriz@gmail.com>
2018-03-15 21:50:38 -0500

    Fix DescriptorSetLayoutBinding marshalling

--- src/DescriptorSetLayout.cpp
+++ src/DescriptorSetLayout.cpp
@@ -7,6 +7,7 @@ void vk::DescriptorSetLayoutBinding::marshal() const {
     m_info.binding = binding;
     m_info.descriptorType = static_cast<VkDescriptorType>(descriptorType);
     m_info.descriptorCount = static_cast<uint32_t>(descriptorCount);
+    m_info.stageFlags = static_cast<VkShaderStageFlags>(stageFlags);
 
     m_samplers.reserve(immutableSamplers.size());
     for (const vk::Sampler& sampler : immutableSamplers) {
